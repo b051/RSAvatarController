79e5f4a92b1908d2e04fd0a843f8b59ac2c66321
Alexis Gallagher <alexis@alexisgallagher.com>
2014-10-03 11:35:01 -0700

    demo view controller displays picked,moved, and scaled image

--- RSAvatarController/DemoViewController.m
+++ RSAvatarController/DemoViewController.m
@@ -11,6 +11,18 @@
 
 @interface DemoViewController ()
 @property (strong,nonatomic) RSAvatarController * rsAvatarController;
+@property (weak,nonatomic) UIImageView * pickedImageView;
+@end
+
+@interface DemoViewController  (RSAvatarControllerDelegate) <RSAvatarControllerDelegate>
+- (void)avatarController:(RSAvatarController *)controller pickedAvatar:(UIImage *)avatar;
+- (UIView *)avatarController:(RSAvatarController *)controller overlayForMoveAndScale:(id<RSMoveAndScaleTrait>)trait;
+- (CGRect)popoverRectForAvatarController:(RSAvatarController *)controller;
+- (CGSize)destinationImageSizeForAvatarController:(RSAvatarController *)controller;
+
+//@optional
+- (UIView *)overlayForAvatarControllerImagePicker:(RSAvatarController *)controller;
+
 @end
 
 @implementation DemoViewController
@@ -25,25 +37,31 @@
   [button setTitle:@"LAUNCH RSAVATARCONTROLLER" forState:UIControlStateNormal];
   [button addTarget:self action:@selector(handleTap:) forControlEvents:UIControlEventTouchUpInside];
   [self.view addSubview:button];
+  
+  // add a UIImageView beneath it.
+  UIImageView * imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+  [self.view addSubview:imageView];
+  self.pickedImageView = imageView;
 
   // center the button
   button.translatesAutoresizingMaskIntoConstraints = NO;
-  NSLayoutConstraint * centerX = [NSLayoutConstraint constraintWithItem:button
-                                                        attribute:NSLayoutAttributeCenterX
-                                                        relatedBy:NSLayoutRelationEqual toItem:self.view
-                                                        attribute:NSLayoutAttributeCenterX
-                                                       multiplier:1.0 constant:0];
-  NSLayoutConstraint * centerY = [NSLayoutConstraint constraintWithItem:button
-                                                        attribute:NSLayoutAttributeCenterY
-                                                        relatedBy:NSLayoutRelationEqual toItem:self.view
-                                                        attribute:NSLayoutAttributeCenterY
-                                                       multiplier:1.0 constant:0];
-  [self.view addConstraints:@[centerX,centerY]];
+  imageView.translatesAutoresizingMaskIntoConstraints = NO;
+  UIView * superview = self.view;
+  NSDictionary * views = NSDictionaryOfVariableBindings(button,imageView,superview);
+  [@[@"V:|-(100)-[button]-(20)-[imageView]", // stack the views
+     @"V:[superview]-(>=0)-[button]"]        // center them w/r/t/ superview
+   enumerateObjectsUsingBlock:
+   ^(NSString * visualFormat, NSUInteger idx, BOOL *stop) {
+     [self.view addConstraints:
+      [NSLayoutConstraint constraintsWithVisualFormat:visualFormat
+                                              options:NSLayoutFormatAlignAllCenterX
+                                              metrics:nil views:views]];
+   }];
 }
-
 
 - (void) handleTap:(id)sender {
   self.rsAvatarController = [[RSAvatarController alloc] init];
+  self.rsAvatarController.delegate = self;
   [self.rsAvatarController openActionSheetInController:self];
 }
 
@@ -52,14 +70,73 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - RSAvatarControllerDelegate
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)avatarController:(RSAvatarController *)controller pickedAvatar:(UIImage *)avatar
+{
+  NSLog(@"picked image=%@",avatar);
+  self.pickedImageView.image = avatar;
 }
-*/
 
+- (UIView *)avatarController:(RSAvatarController *)controller
+      overlayForMoveAndScale:(id<RSMoveAndScaleTrait>)trait
+{
+  /* 
+   setup a view holding a "cancel" and "choose" button
+   */
+  UIView * moveAndScaleOverlay = [[UIView alloc] initWithFrame:CGRectMake(50, 50, 200, 100)];
+  
+  UIButton * cancelButton = [UIButton buttonWithType:UIButtonTypeSystem];
+  [cancelButton setTitle:@"cancel" forState:UIControlStateNormal];
+  
+  UIButton * chooseButton = [UIButton buttonWithType:UIButtonTypeSystem];
+  [chooseButton setTitle:@"choose" forState:UIControlStateNormal];
+
+  // wire up the buttons to call the trait object
+  [cancelButton addTarget:trait action:@selector(cancel) forControlEvents:UIControlEventTouchUpInside];
+  [chooseButton addTarget:trait action:@selector(choose) forControlEvents:UIControlEventTouchUpInside];
+  
+
+  // set up layout so the buttons are left to right with a bit of padding
+  [moveAndScaleOverlay addSubview:cancelButton];
+  [moveAndScaleOverlay addSubview:chooseButton];
+  
+  NSDictionary * views = NSDictionaryOfVariableBindings(cancelButton,chooseButton);
+  NSMutableArray * constraints = [NSMutableArray array];
+  [constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[cancelButton]-(>=20)-[chooseButton]-|" options:NSLayoutFormatAlignAllCenterY metrics:nil views:views]];
+  [constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=0)-[cancelButton]-(>=0)-|" options:0 metrics:nil views:views]];
+  [constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=0)-[chooseButton]-(>=0)-|" options:0 metrics:nil views:views]];
+  cancelButton.translatesAutoresizingMaskIntoConstraints = NO;
+  chooseButton.translatesAutoresizingMaskIntoConstraints = NO;
+  [moveAndScaleOverlay addConstraints:constraints];
+  
+  CGSize smallerSize = [moveAndScaleOverlay systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];
+  moveAndScaleOverlay.frame = CGRectMake(moveAndScaleOverlay.frame.origin.x,moveAndScaleOverlay.frame.origin.y,
+                                         smallerSize.width,smallerSize.height);
+  
+  return moveAndScaleOverlay;
+}
+
+- (CGRect)popoverRectForAvatarController:(RSAvatarController *)controller
+{
+  // TODO: implement example for iPad
+  return CGRectMake(0, 0, 100, 100);
+}
+
+- (CGSize)destinationImageSizeForAvatarController:(RSAvatarController *)controller
+{
+  return CGSizeMake(150, 150);
+}
+
+//@optional
+//- (UIView *)overlayForAvatarControllerImagePicker:(RSAvatarController *)controller
+//{
+//  return nil;
+//}
+
+#pragma mark debugging
+
+-(void)logTap:(id)sender {
+  NSLog(@"tap on %@",sender);
+}
 @end
